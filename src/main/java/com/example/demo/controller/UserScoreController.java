52900edf5e7bfe0a57eba06743a52b75f2afb5e7
Dipali Pundir <dipali.pundir@gdn-commerce.com>
2019-03-08 18:49:22 +0530

    controller service

--- src/main/java/com/example/demo/controller/UserScoreController.java
+++ src/main/java/com/example/demo/controller/UserScoreController.java
@@ -1,9 +1,41 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.UserScoreDTO;
+import com.example.demo.model.UserScoreEntity;
+import com.example.demo.service.UserScoreService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/score")
 public class UserScoreController {
+
+    @Autowired
+    private UserScoreService userScoreService;
+
+    @RequestMapping(value = "/add",method = RequestMethod.GET)
+    public UserScoreEntity addScore(UserScoreDTO userScoreDTO)
+    {
+        return userScoreService.addScore(userScoreDTO);
+    }
+
+    @RequestMapping(value = "/add",method = RequestMethod.GET)
+    public UserScoreEntity getScore(String id)
+    {
+        return userScoreService.getScore(id);
+    }
+
+    @RequestMapping(value = "/add",method = RequestMethod.GET)
+    public UserScoreEntity updateScore(UserScoreDTO userScoreDTO)
+    {
+        return userScoreService.updateScore(userScoreDTO);
+    }
+
+    @RequestMapping(value = "/add",method = RequestMethod.GET)
+    public void deleteScore(String id)
+    {
+         userScoreService.deleteScore(id);
+    }
 }
